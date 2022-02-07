5a3aafbfa31c073ba6dccede85a4326149e47760
MeeM05 <meemchan05@gmail.com>
2022-02-07 10:01:45 -0600

    BUILD 1
    horizontal movement implemented

--- top down game/Assets/Scripts/PlayerMovement.cs
+++ top down game/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+	//Component references, stuff like Rigidbody, animator, and SpriteRenderer go in here
+	public Rigidbody2D rb;
+
+
+    //Movement variables
+    private float horizVelocity;
+    private float vertVelocity; 
+	public float speed = 4f;
+
+
+    //Dash variables
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveH();
+    }
+
+
+    void MoveH()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        horizVelocity = x * speed;
+        rb.velocity = new Vector2(horizVelocity, rb.velocity.y);
+
     }
 }
