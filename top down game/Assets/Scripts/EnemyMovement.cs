a98bbc3aa158ad5f532d1127cb8d308da7681a4d
MeeM05 <meemchan05@gmail.com>
2022-02-22 09:52:53 -0600

    enemy movement update

--- top down game/Assets/Scripts/EnemyMovement.cs
+++ top down game/Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float speed;
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
+//0:43
